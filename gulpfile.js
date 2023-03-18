b228092da44304a386e3fc4b4c366e0ab943654a
Vo7kov <drainys1234@gmail.com>
2023-03-18 17:12:43 +0100

    gulp structure was added

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const del = require('del');
+
+function browsersync() {
+  browserSync.init({
+    server: { baseDir: 'src/' },
+    notify: false,
+    online: false
+  });
+}
+
+function scripts() {
+  return src([
+    'src/scripts/main.js'
+  ])
+
+  .pipe(concat('main.min.js'))
+  .pipe(dest('src/scripts/'))
+
+  .pipe(browserSync.stream())
+}
+
+function styles() {
+  return src([
+    'src/styles/main.scss'
+  ])
+
+  .pipe(sass())
+  .pipe(concat('main.min.css'))
+  .pipe(dest('src/css/'))
+
+  .pipe(browserSync.stream())
+}
+
+function images() {
+  return src('src/images/src/**/*')
+
+  .pipe(newer('src/images/dest/'))
+  .pipe(imagemin())
+  .pipe(dest('src/images/dest/'))
+}
+
+function cleanimg() {
+  return del('src/images/dest/**/*', { force: true })
+}
+
+function cleandist() {
+  return del('dist/**/*', { force: true })
+}
+
+function buildcopy() {
+  return src([
+    'src/css/**/*.min.css',
+    'src/scripts/**/*.min.js',
+    'src/images/dest/**/*',
+    'src/**/*.html',
+  ], { base: 'src' })
+
+  .pipe(dest('dist'));
+}
+
+function startWatch() {
+  watch('src/styles/**/*', styles);
+  watch(['src/**/*.js', '!src/**/*.min.js'], scripts);
+  watch('src/**/*.html').on('change', browserSync.reload);
+  watch('src/images/src/**/*', images);
+}
+
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.images = images;
+exports.cleanimg = cleanimg;
+exports.build = series(cleandist, styles, scripts, images, buildcopy);
+
+exports.default = parallel(scripts, styles, browsersync, startWatch);
