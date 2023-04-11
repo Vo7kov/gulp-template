50b6ff0e9838cf87998e3ec3f06f301223d14cb7
Vo7kov <drainys1234@gmail.com>
2023-04-11 22:50:57 +0300

    add new stylelint rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,9 +3,206 @@ module.exports = {
     "stylelint-config-recommended-scss",
   ],
   plugins: [
-    "stylelint-scss"
+    "stylelint-scss",
+    "stylelint-order",
   ],
   "rules": {
+    "order/properties-order": [
+      {
+        "groupName": "normalize",
+        "emptyLineBefore": "always",
+        "properties": [
+          "box-sizing",
+          "scroll-behavior",
+          "scroll-snap-coordinate",
+          "scroll-snap-destination",
+          "scroll-snap-type",
+          "scroll-snap-type-x",
+          "scroll-snap-type-y"
+        ]
+      },
+
+      {
+        "groupName": "blockModel",
+        "emptyLineBefore": "always",
+        "properties": [
+          "margin",
+          "margin-top",
+          "margin-left",
+          "margin-right",
+          "margin-bottom",
+          "padding",
+          "padding-top",
+          "padding-left",
+          "padding-right",
+          "padding-bottom",
+          "display",
+          "width",
+          "min-width",
+          "max-width",
+          "height",
+          "min-height",
+          "max-width",
+        ]
+      },
+
+      {
+        "groupName": "positioning",
+        "emptyLineBefore": "always",
+        "properties": [
+          "position",
+          "top",
+          "left",
+          "right",
+          "bottom",
+          "z-index",
+        ]
+      },
+
+      {
+        "groupName": "grid",
+        "emptyLineBefore": "always",
+        "properties": [
+          "grid",
+          "grid-template-columns",
+          "grid-template-rows",
+          "column-gap",
+          "grid-column",
+          "grid-row",
+          "justify-content",
+          "justify-items",
+          "align-content",
+          "align-items",
+        ]
+      },
+
+      {
+        "groupName": "flex",
+        "emptyLineBefore": "always",
+        "properties": [
+          "flex",
+          "flex-direction",
+          "flex-wrap",
+          "flex-grow",
+          "flex-shrink",
+          "flex-basis",
+          "order",
+        ]
+      },
+
+      {
+        "groupName": "typography",
+        "emptyLineBefore": "always",
+        "properties": [
+          "font-family",
+          "font-style",
+          "font-weight",
+          "font-size",
+          "line-height",
+          "text-align",
+          "color",
+        ]
+      },
+
+      {
+        "groupName": "decor",
+        "emptyLineBefore": "always",
+        "properties": [
+          "background-color",
+          "border",
+          "border-top",
+          "border-left",
+          "border-right",
+          "border-bottom",
+          "border-radius",
+          "opacity",
+          "list-style",
+          "list-style-type",
+          "list-style-position",
+          "list-style-image",
+        ]
+      },
+
+      {
+        "groupName": "backgroundImage",
+        "emptyLineBefore": "always",
+        "properties": [
+          "background",
+          "background-image",
+          "background-position",
+          "background-position-x",
+          "background-position-y",
+          "background-size",
+          "background-repeat",
+          "background-repeat-x",
+          "background-repeat-y",
+          "background-origin",
+          "background-clip",
+          "background-attachment",
+          "background-color",
+          "background-blend-mode",
+          "image-orientation",
+          "image-rendering",
+          "object-fit",
+          "object-position",
+        ]
+      },
+
+      {
+        "groupName": "animation",
+        "emptyLineBefore": "always",
+        "properties": [
+          "transition",
+          "transition-property",
+          "transition-duration",
+          "transition-delay",
+          "transition-timing-function",
+          "animation",
+          "animation-name",
+          "animation-duration",
+          "animation-delay",
+          "animation-timing-function",
+          "animation-iteration-count",
+          "animation-direction",
+          "animation-fill-mode",
+          "animation-play-state",
+        ]
+      },
+
+      {
+        "groupName": "transform",
+        "emptyLineBefore": "always",
+        "properties": [
+          "transform",
+          "transform-box",
+          "transform-origin",
+          "transform-origin-x",
+          "transform-origin-y",
+          "transform-origin-z",
+          "transform-style",
+        ]
+      },
+
+      {
+        "groupName": "other",
+        "emptyLineBefore": "always",
+        "properties": [
+          "overflow",
+          "overflow-x",
+          "overflow-y",
+          "will-change",
+          "cursor",
+          "pointer-events",
+          "touch-action",
+          "user-drag",
+          "user-focus",
+          "user-select",
+          "user-zoom",
+          "resize",
+        ]
+      },
+    ],
+
     "color-hex-case": "lower",
     "color-hex-length": "short",
     "color-no-invalid-hex": true,
@@ -78,6 +275,6 @@ module.exports = {
     "max-empty-lines": 1,
     "max-line-length": 80,
     "no-extra-semicolons": true,
-    "no-missing-end-of-source-newline": true
+    "no-missing-end-of-source-newline": true,
   }
 };
